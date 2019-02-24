7a0d7cda8f3fae83b12338fe99057c4c170c88e1
Umar Khan <umarkhandev@gmail.com>
2019-02-24 21:35:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,40 +1,43 @@
+def loop_iterator(number_of_times)
 phrase = "Welcome to Flatiron School's Web Development Course!"
-
-  def loop_iterator(number_of_times)
   counter= 0
   loop do
     puts phrase
       counter += 1
       if counter >= 7
-      break
+        break
     end
   end
 end
 
 def times_iterator(number_of_times)
-  7.times do
-  puts phrase
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+    7.times do
+      puts phrase
   end
 end
 
 def while_iterator(number_of_times)
-  counter = 0
-  while counter < 7
-    puts phrase
-    counter += 1
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+    counter = 0
+    while counter < 7
+      puts phrase
+      counter += 1
   end
 end
 
 def until_iterator(number_of_times)
-  counter = 0
-  until counter == 7
-    puts phrase
-    counter += 1
-  end
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+    counter = 0
+      until counter == 7
+          puts phrase
+        counter += 1
+    end
 end
 
 def for_iterator(number_of_times)
-  for counter in 1..7 do
-    puts phrase
-  end
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+    for counter in 1..7 do
+      puts phrase
+    end
 end
